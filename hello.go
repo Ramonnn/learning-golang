962f2402b20f4d4b58c47e0e4302ef7182e4146b
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-01-25 19:53:23 +0100

    hello world complete

--- hello.go
+++ hello.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	frenchHelloPrefix  = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+	englishHelloPrefix = "Hello, "
+	spanish            = "Spanish"
+	french             = "French"
+)
 
-func Hello(name string) string {
-  if name == "" {
-    name = "World"
-  }
-  return englishHelloPrefix + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-  fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
